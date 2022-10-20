e75507fca8921f03ca9f89d375f5071c40a89c58
Boyan Atanasov <b.atanasov@icloud.com>
2022-10-20 18:23:54 +0100

    greedy match for prefix tree

--- src/PrefixTree.hs
+++ src/PrefixTree.hs
@@ -27,6 +27,13 @@ insert (ch : chrest) (PrefixTree nodes eow) = case Map.lookup ch nodes of
   Just ptree -> PrefixTree (Map.insert ch (insert chrest ptree) nodes) eow
   Nothing -> PrefixTree (Map.insert ch (insert chrest (empty :: PrefixTree)) nodes) eow
 
+combine :: PrefixMatch -> Maybe PrefixMatch -> PrefixMatch
+combine pml@(PrefixMatch _ _) Nothing = pml
+combine (PrefixMatch loffset lptree) (Just (PrefixMatch roffset rptree)) =
+  PrefixMatch {offset = loffset + roffset, ptree = rptree}
+
+(//>) = combine
+
 match :: [Char] -> PrefixTree -> Maybe PrefixMatch
 match [] _ = Nothing
 match chars@(ch : chrest) ptree@(PrefixTree nodes eow) = match' chars ptree 0
@@ -36,4 +43,14 @@ match chars@(ch : chrest) ptree@(PrefixTree nodes eow) = match' chars ptree 0
       Just ptree@(PrefixTree _ eow)
         | eow -> Just (PrefixMatch (offset + 1) ptree)
         | otherwise -> match' chrest ptree (offset + 1)
+      Nothing -> Nothing
+
+matchgreedy :: [Char] -> PrefixTree -> Maybe PrefixMatch
+matchgreedy [] _ = Nothing
+matchgreedy chars ptree = matchgreedy' chars ptree Nothing
+  where
+    matchgreedy' [] _ _ = Nothing
+    matchgreedy' chars ptree pm = case match chars ptree of
+      Just pm@(PrefixMatch offset ptree) -> Just $ pm //> 
+        matchgreedy' (drop offset chars) ptree (Just pm)
       Nothing -> Nothing
